79c06fa766887e2f2b96df84c0aaa8b4d19d3431
SAMApl <onelolkpiece@gmail.com>
2025-10-28 13:12:56 +0500

    Add files via upload

--- pythonbo.py
+++ pythonbo.py
@@ -4,7 +4,7 @@ from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTyp
 TOKEN = "8487910255:AAHe5WmQO5yDx7XSbboSjOHymVGXbvnyxpw"
 
 TARGET_USER_ID = 5420403420  
-GIF_URL = "https://media.giphy.com/media/v1.Y2lkPWVjZjA1ZTQ3eWZ1M3VqcnJja2s5NW1xb3AxN2JyYXE1aHVpMWNycTdrMnA3ZzNrciZlcD12MV9naWZzX3NlYXJjaCZjdD1n/LjXVOmTvaWj8GWkfy8/giphy.gif"
+GIF_URL = "https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExdXg0YjUycGR2cTNnYjYwcGR3M3p3MGk5NzFkMnBlYzBkZXNzY2JqeSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/YV6rWBvUkWHF39V1sZ/giphy.gif"
 async def on_member_leave(update: Update, context: ContextTypes.DEFAULT_TYPE):
     member = update.message.left_chat_member
     if member.id == TARGET_USER_ID:
